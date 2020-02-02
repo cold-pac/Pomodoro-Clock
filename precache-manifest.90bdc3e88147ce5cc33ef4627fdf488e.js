66e6cbc819f51841298902e8ce338803fd96e4f2
Erol Cimen <erol.a.cimen@gmail.com>
2020-02-02 23:08:34 +1100

    Updates

---
+++ precache-manifest.90bdc3e88147ce5cc33ef4627fdf488e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0f3b6e90004c82c51fe1ebb2f0dcf3",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "4f74dc867301a26d7181",
+    "url": "/Pomodoro-Clock/static/js/2.95551735.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Pomodoro-Clock/static/js/2.95551735.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9cd452008cb22b934566",
+    "url": "/Pomodoro-Clock/static/js/main.7e8e7ba7.chunk.js"
+  },
+  {
+    "revision": "e61fbdd37a0993dd0104",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.2ad0bfd1.js"
+  }
+]);
